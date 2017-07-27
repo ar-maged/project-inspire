ab732d4936767f0edee9a387053bf1b78c1fa8fd
Maged <abdulrahman.maged95@gmail.com>
2017-07-27 18:55:57 +0200

    :wrench: Refine code

--- js/main.js
+++ js/main.js
@@ -1,42 +1,52 @@
-$(document).ready(function() {
+$(document).ready(() => {
+  // Initialize state
+  const state = {
+    currentColorIndex: 0,
+    isInitialClick: true,
+  };
+
   // Animate "Click something."
   $('h2').typeIt();
 
   // Activate tooltipster
   $('.tooltip').tooltipster({
-    offsetY: -95,
     theme: 'tooltipster-light',
+    offsetY: -95,
   });
 
-  $(document).on('click', function() {
-    // Initially reduce h1 font size
-    if (index === 0) {
+  $(document).on('click', () => {
+    // Initially reduce <h1> font size
+    if (state.isInitialClick) {
       $('h1').css('font-size', '50px');
+      state.isInitialClick = false;
     }
 
-    // Update background colour with animation
-    $('body').animate({
-      backgroundColor: colors[index],
-    });
-
-    if (index === colors.length - 1) {
-      index = 0;
+    // Increment/reset color index
+    if (state.currentColorIndex === colors.length - 1) {
+      state.currentColorIndex = 0;
     } else {
-      index++;
+      state.currentColorIndex++;
     }
 
-    // Update quote
-    var randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+    // Pick random quote index
+    const randomQuoteIndex = Math.floor(Math.random() * quotes.length);
 
+    // Select quote with random index
+    const randomQuote = quotes[randomQuoteIndex];
+
+    // Update quote text & author
     $('h1').text('“' + randomQuote.text + '”');
     $('h2').text(randomQuote.author);
+
+    // Update tooltip
+    $('h2').tooltipster('content', randomQuote.description);
 
     // Animate author name
     $('h2').typeIt();
 
-    // Update tooltip
-    $('h2').tooltipster('content', randomQuote.description);
+    // Update background colour with animation
+    $('body').animate({
+      backgroundColor: colors[state.currentColorIndex],
+    });
   });
 });
-
-var index = 0;
